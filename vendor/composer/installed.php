a4b03cb7a0ea7813e61bb05cb97c0dff1d2c9b16
Andrzej Piotrowski <apiotrowski@whitelabelcoders.com>
2025-01-07 12:08:02 +0100

    add vendor with no dev

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wlc/promoted-product',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b68d659bc42edf84795307d422c20a855b2083ae',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wlc/promoted-product' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b68d659bc42edf84795307d422c20a855b2083ae',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
